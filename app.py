f41be06bb87fca2c62fd7154664ce3e293a63633
dreilly710 <dreilly1@terpmail.umd.edu>
2026-02-06 15:37:07 -0500

    git commit -m "Add homepage route and update Flask app"

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Hello World from Daniel Reilly! I am adding my first code change."
+
+
 #Hello World route
 @app.route("/hello")
 def hello():
