07174dbf5dae8b605fb284b35a7d2479a4cd6ee0
dreilly710 <dreilly1@terpmail.umd.edu>
2026-02-20 11:49:14 -0500

    git commit -m "Add about page template"

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def index():
 def hello():
     return render_template("hello.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
