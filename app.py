ec6dbfe28aee899188814adb2fb5a2335abde413
dreilly710 <dreilly1@terpmail.umd.edu>
2026-03-06 15:22:58 -0500

    Fix index route to render template

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World from Daniel Reilly! I am adding my first code change."
+    return render_template("hello.html")
 
 
 #Hello World route
